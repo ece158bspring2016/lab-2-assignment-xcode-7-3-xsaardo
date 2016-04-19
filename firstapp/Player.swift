f7d532c484af16c554db274185efb99f01943374
Cuong Luong <cuongluong@Cuongs-Mac.local>
2016-04-19 00:13:44 -0700

    First

---
+++ firstapp/Player.swift
@@ -0,0 +1,9 @@
+//
+//  Player.swift
+//  firstapp
+//
+//  Created by Cuong Luong on 4/18/16.
+//  Copyright © 2016 Cuong Luong. All rights reserved.
+//
+
+import Foundation
---
+++ firstapp/SampleData.swift
@@ -0,0 +1,9 @@
+//
+//  SampleData.swift
+//  firstapp
+//
+//  Created by Cuong Luong on 4/18/16.
+//  Copyright © 2016 Cuong Luong. All rights reserved.
+//
+
+import Foundation
